d584f3f2bf8adad4c92b6936c2a1a075d8f774ad
Darryl Brooks <Darryl.Brooks@myfuelmaster.com>
2019-10-19 19:09:38 -0400

    Initial Commit

--- priority_school_tasker/lib/main.dart
+++ priority_school_tasker/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+from canvasapi import Canvas;
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< Updated upstream
       title: 'Priority School Tasker',
+=======
+      title: 'Priority Task Manager',
+>>>>>>> Stashed changes
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -21,19 +26,51 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
+<<<<<<< Updated upstream
       home: Scaffold
       (
         appBar: new AppBar(title: Text("Dynamic List View"),),
         body: MyDynamicListView(),
       ), //Scaffold
     ); //MyMaterialApp
+=======
+      home: MyHomePage(title: 'Tasks'),
+    );
+>>>>>>> Stashed changes
   }
 }
 
 class MyDynamicListView extends StatelessWidget {
 
+<<<<<<< Updated upstream
+=======
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter = _counter + 2;
+    });
+  }
+>>>>>>> Stashed changes
 
   @override
   Widget build(BuildContext context) {
