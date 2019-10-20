64a730f0395ba9b155d7d2f4dd4314342a7b2642
Darryl Brooks <Darryl.Brooks@myfuelmaster.com>
2019-10-19 21:01:41 -0400

    Grabs Courses & Upcoming Events

--- priority_school_tasker/lib/taskImporter.py
+++ priority_school_tasker/lib/taskImporter.py
@@ -1,15 +1,22 @@
 from canvasapi import Canvas
+import requests
 
 # Canvas API URL
 API_URL = "https://famu-fsu-eng.instructure.com"
-# Canvas API key
+
+# Canvas API key **** Will be unique per user
 API_KEY = "12564~DnjktTW4xXJC6LPoU1S398ZaVZy4qJD9e2hVoes3tMjLks03JvrJZ91r1PkHMlBb"
 
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
 
-courseNum = canvas.get_course(284)
+#Pulls id for current user
+currentUser = canvas.get_current_user()
 
-print("Course Number: " + str(courseNum))
+#Pulls upcoming events
+upComingEvents = requests.get(API_URL + "/api/v1/calendar_events?access_token=" + API_KEY)
+
+
+
 
 
