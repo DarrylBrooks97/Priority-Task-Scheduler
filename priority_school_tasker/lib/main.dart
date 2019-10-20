60f271fd1692fb093b63dace40513eafa5e7b680
TheNextCEO <charles.benji33@gmail.com>
2019-10-20 12:48:53 -0400

    Add Task Containers

--- priority_school_tasker/lib/main.dart
+++ priority_school_tasker/lib/main.dart
@@ -173,41 +173,41 @@ class Tasks extends StatelessWidget {
         padding: const EdgeInsets.all(8),
         children: <Widget>[
           Container(
-            height: 50,
+            height: 100,
             color: Colors.orange[900],
             child: const Center(child: Text('Task 1 Name' +
                 '\n\nClass' + '\n\nDate Due')),
           ),
           Container(
-            height: 50,
+            height: 100,
             color: Colors.orange[800],
             child: const Center(child: Text('Task 2 Name' +
                 '\n\nClass' + '\n\nDate Due')),
 
           ),
           Container(
-            height: 50,
+            height: 100,
             color: Colors.orange[700],
             child: const Center(child: Text('Task 3 Name' +
                 '\n\nClass' + '\n\nDate Due')),
 
           ),
           Container(
-            height: 50,
+            height: 100,
             color: Colors.orange[600],
             child: const Center(child: Text('Task 4 Name' +
                 '\n\nClass' + '\n\nDate Due')),
 
           ),
           Container(
-            height: 50,
+            height: 100,
             color: Colors.orange[500],
             child: const Center(child: Text('Task 5 Name' +
                 '\n\nClass' + '\n\nDate Due')),
 
           ),
           Container(
-            height: 50,
+            height: 100,
             color: Colors.orange[400],
             child: const Center(child: Text('Task 6 Name' +
                 '\n\nClass' + '\n\nDate Due')),
