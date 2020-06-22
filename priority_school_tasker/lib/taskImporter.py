05c20d798dceaf930aadf7563fedfb63bdfa5985
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-06-22 10:49:20 -0400

    Updated README.md

--- priority_school_tasker/lib/taskImporter.py
+++ priority_school_tasker/lib/taskImporter.py
@@ -5,7 +5,7 @@ import requests
 API_URL = "https://famu-fsu-eng.instructure.com"
 
 # Canvas API key **** Will be unique per user
-API_KEY = "12564~DnjktTW4xXJC6LPoU1S398ZaVZy4qJD9e2hVoes3tMjLks03JvrJZ91r1PkHMlBb"
+API_KEY = "<YOUR-API-KEY>"
 
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
